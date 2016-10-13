488f45541f175221db23bb28e427de7f0f7d7046
ghostffcode <meremezeukah@gmail.com>
2016-10-13 13:51:24 -0400

    added env configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,31 @@ const path = require('path');
 const config = require('./package.json');
 
 const webpack = require('webpack');
+require('dotenv').config();
+
+const PROD = process.env.NODE_ENV === 'production';
+
+let plugins = [];
+
+PROD ? [
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }))
+  ] : '';
 
 module.exports = {
   entry: path.resolve(__dirname, config.main),
   devtool: 'source-map',
   output: {
+    library: process.env.NAME,
+    libraryTarget: process.env.TARGET,
     path: __dirname,
-    filename: 'build/bundle.js'
+    filename: (PROD) ? 'build/ghost.min.js' : 'build/ghost.js'
   },
   module: {
     loaders: [
       {test: /\.es6?$/, exclude: /node_modules/, loader: 'babel'}
     ]
-  }
+  },
+  plugins: plugins
 };
